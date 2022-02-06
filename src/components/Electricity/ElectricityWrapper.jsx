73c6989435720592fe54f4cd4b31205cd7930c2f
Beezay Bohora <59993093+beezay@users.noreply.github.com>
2022-02-06 21:42:23 +0545

    project init

---
+++ src/components/Electricity/ElectricityWrapper.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ElectricityWrapper = () => {
+  return (
+    <div>
+      <p>This is Electricity Page Wrapper</p>
+    </div>
+  );
+};
+
+export default ElectricityWrapper;
